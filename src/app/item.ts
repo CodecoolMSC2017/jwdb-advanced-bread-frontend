9a2e29712216d71c884147475f3c0a849e766877
zsarna2 <zsarna2@gmail.com>
2018-08-07 13:49:18 +0200

    item and ingredient class implemented

---
+++ src/app/item.ts
@@ -0,0 +1,10 @@
+import { Ingredient } from "./ingredient";
+import { Restaurant } from "./restaurant";
+
+export class Item {
+    name : String;
+    price : Number;
+    category : String;
+    ingredients : Array<Ingredient>;
+    restaurant : Restaurant;
+}
